44523f6f50c1932a24ea12d239760861598c55fa
lgklsv <101424508+lgklsv@users.noreply.github.com>
2022-07-28 23:09:51 +0300

    multiple pages language switcher

--- scripts/lang-app.js
+++ scripts/lang-app.js
@@ -2,11 +2,8 @@
 const langSwitch = document.querySelector('.lang-switch');
 const allLang = ['ru', 'en'];
 
-location.href = window.location.pathname + '#ru';
-changeLanguage();
-
-langSwitch.addEventListener('click', btnLanguageSwitcher);
-langSwitch.addEventListener('click', changeUrlLang);
+document.addEventListener('DOMContentLoaded', getLocalLang)
+langSwitch.addEventListener('click', changeLanguage);
 
 function btnLanguageSwitcher() {
     if (langSwitch.textContent === 'Русский') {
@@ -19,22 +16,62 @@ function btnLanguageSwitcher() {
     }
 }
 
-function changeUrlLang() {
+function changeLanguage() {
+    btnLanguageSwitcher()
     let lang = langSwitch.value;
     location.href = window.location.pathname + '#' + lang;
-    changeLanguage();
-}
-
-function changeLanguage() {
     let hash = window.location.hash;
     hash = hash.slice(1);
     if (!allLang.includes(hash)) {
         location.href = window.location.pathname + '#ru';
     }
+    saveLocalLang(hash);
     for (let key in langArr) {
         let elem = document.querySelector('.lng-'+ key);
         if (elem) {
             elem.textContent = langArr[key][hash];
         }
     }
+}
+
+function saveLocalLang(language) {
+    let langs;
+    if(localStorage.getItem('langs') === null) {
+        langs = [];
+    } else {
+        langs = JSON.parse(localStorage.getItem('langs'));
+    }
+    langs.push(language);
+    localStorage.setItem('langs', JSON.stringify(langs));
+}
+
+function getLocalLang() {
+    let langs;
+    if (localStorage.getItem('langs') === null) {
+        langs = [];
+        location.href = window.location.pathname + '#ru';
+        langSwitch.textContent = 'English';
+        langSwitch.value = 'ru';
+    } else {
+        langs = JSON.parse(localStorage.getItem('langs'));
+        let hash = langs[langs.length - 1];
+        if (hash === 'ru'){
+            langSwitch.textContent = 'English';
+            langSwitch.value = hash;
+            location.href = window.location.pathname + '#' + hash;
+        } else if (hash === 'en') {
+            langSwitch.textContent = 'Русский';
+            langSwitch.value = hash;
+            location.href = window.location.pathname + '#' + hash;
+        }
+    }
+    langs.forEach(function (language) {
+        let hash = langs[langs.length - 1];
+        for (let key in langArr) {
+            let elem = document.querySelector('.lng-'+ key);
+            if (elem) {
+                elem.textContent = langArr[key][hash];
+            }
+        }
+    });
 }
