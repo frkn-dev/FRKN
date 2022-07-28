6dfa7c5551aa1426ae3cf104b086aed499ed634d
lgklsv <101424508+lgklsv@users.noreply.github.com>
2022-07-28 18:15:32 +0300

    language switcher from one page (#70)

---
+++ scripts/lang.js
@@ -0,0 +1,39 @@
+const langArr = {
+    'header-slogan': {
+        'ru' : 'Роскомнадзор, иди на хуй!',
+        'en' : 'Roskomnadzor, go fuck yourself!',
+    },
+    'welcome' : {
+        'ru' : 'Cвободный VPN для свободных людей.',
+        'en' : 'Free VPN for free humans',
+    },
+    'welcome-text1' : {
+        'ru' : 'Мы за свободу слова и против какой-либо цензуры.',
+        'en' : 'We are for freedom of speech and against any kind of censorship.',
+    },
+    'welcome-text2' : {
+        'ru' : 'Делаем некомерческий',
+        'en' : 'We are making a non-commercial',
+    },
+    'welcome-text3' : {
+        'ru' : ', не собирающий никаких данных.',
+        'en' : 'that does not collect any data.',
+    },
+    'welcome-text4' : {
+        'ru' : 'Сегодня свобода слова особенно уязвима, независимые СМИ запрещены, людям промывают мозги пропагандой, сервисы ВПН блокируют, выражать свою позицию и мнение опасно. Поэтому мы взялись за этот проект, он некоммерческий, и здесь не преследуется никакой выгоды. Мы за свободу слова и против какой-либо цензуры. Компания у нас небольшая, но у нас большой потенциал. Вы можете поддержать нас донатами или любым другим вкладом по улучшению сервиса. Даже заведение ишью в проекте сильно нам поможет.',
+        'en' : 'Today, freedom of speech is especially vulnerable. Independent media are banned, people are brainwashed with propaganda, VPN services are blocked, and it is dangerous to express one\'s position and opinion. This is the reason why we took on this project. It is non-commercial, no profit is pursued either. We are rather small, but yet, we have a great potential. You can support us with donations or any other contribution to improve the service. Pulling requests and creating issues also helps us a lot.',
+    },
+    'link-install' : {
+        'ru' : 'Инструкция по установке',
+        'en' : 'Install Instructions',
+    },
+    'donation' : {
+        'ru' : 'Мы open source проект. Вы можете нас поддержать',
+        'en' : 'We are OpenSource. You can support us.',
+    },
+    'contacts' : {
+        'ru' : 'Обратная связь - ',
+        'en' : 'Contacts - ',
+    },
+    
+}
