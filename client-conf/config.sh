e9b46c0169ef372f7f18a4a917e256c626ae10ff
mr.segfault <mr.segfaulto@gmail.com>
2022-08-20 20:26:44 +0300

    linux nm helper: moved ca, client ca and client key files to ~/.pki/FuckRKN

--- client-conf/config.sh
+++ client-conf/config.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+DST_DIR=$HOME/.pki/FuckRKN1
+
 sudo apt-get update
 sudo apt-get install network-manager-strongswan
 
+mkdir -p $DST_DIR > /dev/null
+
+CACERT="$DST_DIR/ca.cer"
+CLIENT_CERT="$DST_DIR/client.cer"
+CLIENT_KEY="$DST_DIR/client.key"
 
 openssl_has_legacy=$(openssl pkcs12 -help 2>&1 | grep legacy)
 
@@ -14,17 +21,11 @@ else
   openssl_legacy_opt='-legacy'
 fi
 
-openssl pkcs12 -in vpnclient.p12 -cacerts -nokeys -out ikev2vpnca.cer $openssl_legacy_opt -password "pass:" 
-openssl pkcs12 -in vpnclient.p12 -clcerts -nokeys -out vpnclient.cer $openssl_legacy_opt  -password "pass:"
-openssl pkcs12 -in vpnclient.p12 -nocerts -nodes  -out vpnclient.key $openssl_legacy_opt  -password "pass:"
+openssl pkcs12 -in vpnclient.p12 -cacerts -nokeys -out $CACERT $openssl_legacy_opt -password "pass:" 
+openssl pkcs12 -in vpnclient.p12 -clcerts -nokeys -out $CLIENT_CERT $openssl_legacy_opt  -password "pass:"
+openssl pkcs12 -in vpnclient.p12 -nocerts -nodes  -out $CLIENT_KEY $openssl_legacy_opt  -password "pass:"
 
+sudo chown $USER.$USER $CACERT $CLIENT_CERT $CLIENT_KEY
+sudo chmod 600 $CACERT $CLIENT_CERT $CLIENT_KEY 
 
-sudo chown root.root ikev2vpnca.cer vpnclient.cer vpnclient.key
-sudo chmod 600 ikev2vpnca.cer vpnclient.cer vpnclient.key
-
-pwd=$(pwd)
-sert=$pwd"/ikev2vpnca.cer"
-usert=$pwd"/vpnclient.cer"
-ukey=$pwd"/vpnclient.key"
-
-nmcli c add type vpn ifname -- vpn-type strongswan connection.id 'fuckRKN1' connection.autoconnect no vpn.data 'address = lt.fuckrkn1.xyz, certificate = '$sert', encap = no, esp = aes128gcm16, ipcomp = no, method = key, proposal = yes, usercert = '$usert', userkey = '$ukey', virtual = yes'
+nmcli c add type vpn ifname -- vpn-type strongswan connection.id 'fuckRKN1' connection.autoconnect no vpn.data 'address = lt.fuckrkn1.xyz, certificate = '$CACERT', encap = no, esp = aes128gcm16, ipcomp = no, method = key, proposal = yes, usercert = '$CLIENT_CERT', userkey = '$CLIENT_KEY', virtual = yes'
