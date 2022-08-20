ebd2f8e09705dc5e86145c7e40eaf70449b68c1a
mr.segfault <mr.segfaulto@gmail.com>
2022-08-20 20:01:02 +0300

    linux nm helper: openssl 1.1.1 support

--- client-conf/config.sh
+++ client-conf/config.sh
@@ -5,9 +5,20 @@ sudo apt-get install network-manager-strongswan
 
 echo -e '\e[33m''Если запрашивает пароль, просто нажать Enter''\e[0m'
 
-openssl pkcs12 -in vpnclient.p12 -cacerts -nokeys -out ikev2vpnca.cer -legacy
-openssl pkcs12 -in vpnclient.p12 -clcerts -nokeys -out vpnclient.cer -legacy
-openssl pkcs12 -in vpnclient.p12 -nocerts -nodes  -out vpnclient.key -legacy
+openssl_has_legacy=$(openssl pkcs12 -help 2>&1 | grep legacy)
+
+if [ -z "$openssl_has_legacy" ]
+then
+  openssl_legacy_opt=''
+  
+else
+  openssl_legacy_opt='-legacy'
+fi
+
+openssl pkcs12 -in vpnclient.p12 -cacerts -nokeys -out ikev2vpnca.cer $openssl_legacy_opt 
+openssl pkcs12 -in vpnclient.p12 -clcerts -nokeys -out vpnclient.cer $openssl_legacy_opt
+openssl pkcs12 -in vpnclient.p12 -nocerts -nodes  -out vpnclient.key $openssl_legacy_opt
+
 rm vpnclient.p12
 
 sudo chown root.root ikev2vpnca.cer vpnclient.cer vpnclient.key
