644619402bb2c0c76f4828a6d8286b7f39346b44
mr.segfault <mr.segfaulto@gmail.com>
2022-08-25 20:32:46 +0300

    linux nm helper: download p12 file from github

--- client-conf/linux-network-manager-config.sh
+++ client-conf/linux-network-manager-config.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 DST_DIR=$HOME/.pki/FuckRKN1
+SRC_URL="https://github.com/nezavisimost/FuckRKN1/raw/main/client-conf/vpnclient.p12"
+NM_CONN_ID='fuckRKN1'
 
 sudo apt-get update
-sudo apt-get install -y network-manager-strongswan
+sudo apt-get install -y network-manager-strongswan wget
+
+wget $SRC_URL -P /tmp
+
+if [ $? != 0 ]; then
+  echo "Failed to download $SRC_URL"
+  exit
+fi
 
 mkdir -p $DST_DIR > /dev/null
 
@@ -21,11 +30,15 @@ else
   openssl_legacy_opt='-legacy'
 fi
 
-openssl pkcs12 -in vpnclient.p12 -cacerts -nokeys -out $CACERT $openssl_legacy_opt -password "pass:" 
-openssl pkcs12 -in vpnclient.p12 -clcerts -nokeys -out $CLIENT_CERT $openssl_legacy_opt  -password "pass:"
-openssl pkcs12 -in vpnclient.p12 -nocerts -nodes  -out $CLIENT_KEY $openssl_legacy_opt  -password "pass:"
+openssl pkcs12 -in /tmp/vpnclient.p12 -cacerts -nokeys -out $CACERT $openssl_legacy_opt -password "pass:" 
+openssl pkcs12 -in /tmp/vpnclient.p12 -clcerts -nokeys -out $CLIENT_CERT $openssl_legacy_opt  -password "pass:"
+openssl pkcs12 -in /tmp/vpnclient.p12 -nocerts -nodes  -out $CLIENT_KEY $openssl_legacy_opt  -password "pass:"
+
+rm /tmp/vpnclient.p12
 
 sudo chown $USER.$USER $CACERT $CLIENT_CERT $CLIENT_KEY
-sudo chmod 600 $CACERT $CLIENT_CERT $CLIENT_KEY 
+sudo chmod 600 $CACERT $CLIENT_CERT $CLIENT_KEY
 
-nmcli c add type vpn ifname -- vpn-type strongswan connection.id 'fuckRKN1' connection.autoconnect no vpn.data 'address = lt.fuckrkn1.xyz, certificate = '$CACERT', encap = no, esp = aes128gcm16, ipcomp = no, method = key, proposal = yes, usercert = '$CLIENT_CERT', userkey = '$CLIENT_KEY', virtual = yes'
+nmcli c delete $NM_CONN_ID > /dev/null 2>&1
+
+nmcli c add type vpn ifname -- vpn-type strongswan connection.id $NM_CONN_ID connection.autoconnect no vpn.data 'address = lt.fuckrkn1.xyz, certificate = '$CACERT', encap = no, esp = aes128gcm16, ipcomp = no, method = key, proposal = yes, usercert = '$CLIENT_CERT', userkey = '$CLIENT_KEY', virtual = yes'
